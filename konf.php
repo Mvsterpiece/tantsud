fa55d921b4f48a46a24ef6d06bbfdf2c78e82fb4
Denis Saiko <93324253+Mvsterpiece@users.noreply.github.com>
2023-02-16 13:46:47 +0200

    Update konf.php

--- konf.php
+++ konf.php
@@ -1,7 +1,7 @@
 <?php
 $baasiaadress="d113372.mysql.zonevs.eu";
 $baasikasutaja="d113372_denis";
-$baasiparool="Armastanminuema";
+$baasiparool="";
 $baasinimi="d113372_baasdenis";
 $yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi);
 ?>
